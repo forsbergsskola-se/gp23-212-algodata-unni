720eebae2e1315a33616f3ea83cd34b83bc37d06
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-05-13 15:52:47 +0200

    Collection exercise

---
+++ Algorithms-And-DataStructures/212_P_1_Collections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System;
+
+List<int> numbers = new List<int>();
+numbers.Add(137);
+numbers.Add(1000);
+numbers.Add(-200);
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+ArrayList arrayList = new ArrayList();
+
+arrayList.Add(true);               
+arrayList.Add("forsbergs");        
+arrayList.Add("a");                
+arrayList.Add(1000);               
+arrayList.Add(.12f);               
+
+for (int i = 0; 1 < arrayList.Count; i++)
+{
+    Console.WriteLine(arrayList[i]);
+}
