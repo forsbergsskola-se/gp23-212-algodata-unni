7dede5465e6f27429c77333fa49dfb44193400d8
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-05-13 15:53:57 +0200

    Create a public static class TurboSort within TurboCollections-Library-Project
    Add a public static void SelectionSort(List<int>)-Method that sorts the List's elements using SelectionSort.

--- Algorithms-And-DataStructures/TurboCollections/TurboSort.cs
+++ Algorithms-And-DataStructures/TurboCollections/TurboSort.cs
@@ -1,25 +1,35 @@
-namespace _213_Selection_Sort;
+using System;
+using System.Collections.Generic;
 
-public class TurboSort
+namespace TurboCollections
 {
-    public static void SelectionSort(List<int> list)             
-    {                                                            
-        for (int i = 0; i < list.Count - 1; i++)                
-        {                                                       
-            int minIndex = i;                                  
-            for (int j = i + 1; j < list.Count; j++)           
-            {                                                  
-                if (list[j] < list[minIndex])                 
-                {                                             
-                    minIndex = j;                            
-                }                                             
-            }                                                  
-                                                             
-            //change the order of the numbers                  
-            int order = list[minIndex];                        
-            list[minIndex] = list[i];                          
-            list[i] = order;                                   
-        }                                                       
-                                                             
-    }                                                            
+    public static class TurboSort
+    {
+        
+        public static void SelectionSort(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[j] < list[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                // Swap the itemst
+                (list[minIndex], list[i]) = (list[i], list[minIndex]);
+            }
+
+    
+        }
+        
+    }
+    
 }
+               
+                                                 
+
+
