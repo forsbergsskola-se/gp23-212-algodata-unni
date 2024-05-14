5c1ec782f036932d698e49f55cdfb4a5ab58294d
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-05-14 16:22:08 +0200

    Turbo math Get even numbers

--- Algorithms-And-DataStructures/TurboCollections/TurboMath.cs
+++ Algorithms-And-DataStructures/TurboCollections/TurboMath.cs
@@ -1,5 +1,23 @@
+namespace TurboCollections;
 
+public class TurboMath
+{
+    public static IEnumerable<int> GetEvenNumbersList(int maxNumber)
+    {
+        for (int i = 0; i < maxNumber; i++)
+        {
+            if (i % 2 ==0)
+            {
+                yield return i;
+            }
+            
+        }
+    }
     
+    
+    public static IEnumerable<int> GetEvenNumbers(int maxNumber){
 
-
-//Funktion som retuns a list 
+    }
+    
+    
+}
