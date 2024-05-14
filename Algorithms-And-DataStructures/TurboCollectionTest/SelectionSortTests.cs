4c680b2b8987780cc9352d6595a4a6a98b514711
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-05-14 16:04:11 +0200

    Sorting test DONE

--- Algorithms-And-DataStructures/TurboCollectionTest/SelectionSortTests.cs
+++ Algorithms-And-DataStructures/TurboCollectionTest/SelectionSortTests.cs
@@ -1,4 +1,3 @@
-
 using TurboCollections;
 
 namespace TurboCollectionTest
@@ -12,8 +11,8 @@ namespace TurboCollectionTest
         {
 
             var list = new List<int> { 5, 3, 1, 2, 4 };
-            TurboSort.SelectionSort(list);
-
+            TurboSort.SelectionSort(list); 
+            CollectionAssert.AreEqual(list, new []{1,2,3,4,5}); // return true or false
         }
     }
 }
