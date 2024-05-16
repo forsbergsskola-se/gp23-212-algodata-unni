549be8564bc5bc036aa843e37680b4393e8cda78
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-05-16 16:23:31 +0200

    PushTest

--- Algorithms-And-DataStructures/TurboCollectionTest/TurboLinkedStackTests.cs
+++ Algorithms-And-DataStructures/TurboCollectionTest/TurboLinkedStackTests.cs
@@ -2,5 +2,11 @@ namespace TurboCollectionTest;
 
 public class TurboLinkedStackTests
 {
+    [Test]
+    public void pushTest()
+    {
+        var addNumber = TurboLinkedStack.Push()
+        
+    }
     
 }
