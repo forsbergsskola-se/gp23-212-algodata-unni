e4522ed58c9f9fd2a7b2e44182f894616150afd0
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-05-20 09:36:00 +0200

    Binary tree test working!

--- Algorithms-And-DataStructures/TurboCollectionTest/TurboBinaryTreeTest.cs
+++ Algorithms-And-DataStructures/TurboCollectionTest/TurboBinaryTreeTest.cs
@@ -1,6 +1,29 @@
-namespace TurboCollectionTest;
+using NUnit.Framework;
+using TurboCollections;
 
-public class TurboBinaryTreeTest
+namespace TurboCollectionTest
 {
-    
+    [TestFixture]
+    public class TurboBinaryTreeTest
+    {
+        [Test]
+        public void TestTurboBinaryTree()
+        {
+            // Arrange new 
+            TurboBinaryTree tree = new TurboBinaryTree();
+            
+            // Data input in node ito tree
+            tree.Insert(10);
+            tree.Insert(5);
+            tree.Insert(15);
+
+            // Assert
+            Assert.NotNull(tree.Root);
+            Assert.AreEqual(10, tree.Root.Data);
+            Assert.NotNull(tree.Root.Left);
+            Assert.AreEqual(5, tree.Root.Left.Data);
+            Assert.NotNull(tree.Root.Right);
+            Assert.AreEqual(15, tree.Root.Right.Data);
+        }
+    }
 }
